ee0bed8738dcaed6ef9db1447607ac5c4932dce2
Aron-Mercado <agmercado@student.apc.edu.ph>
2025-08-11 02:59:25 +0000

    New Base

--- week04/LoopExercise.java
+++ week04/LoopExercise.java
@@ -1,12 +1,23 @@
-// Program to print numbers from 1 to 5
+import java.util.Scanner;
 
 class LoopExercise {
   public static void main(String[] args) {
-  
+    Scanner myObj = new Scanner(System.in);
+
+    System.out.println("Enter two values:");
+
+    // Numerical input
+    int value1 = myObj.nextInt();
+    int value2 = myObj.nextInt();
+
+    // Output input by user
+    System.out.println("First Number a = " + value1);
+    System.out.println("Second Number b = " + value2);
+
     int n = 5;
     // for loop  
     for (int i = 1; i <= n; ++i) {
       System.out.println("Java is fun"+" from: "+i+ " to: "+n);
-    }
+
   }
 }
