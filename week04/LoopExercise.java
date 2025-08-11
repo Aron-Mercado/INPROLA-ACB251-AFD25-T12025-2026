8ea52d6103214ffe3f26df4a61a641ccfcf03b89
Aron-Mercado <agmercado@student.apc.edu.ph>
2025-08-11 03:30:52 +0000

    last try

--- week04/LoopExercise.java
+++ week04/LoopExercise.java
@@ -6,18 +6,32 @@ class LoopExercise {
 
     System.out.println("Enter two values:");
 
+
     // Numerical input
-    int n = myObj.nextInt();
     int i = myObj.nextInt();
+    int n = myObj.nextInt();
+
+    double z = (n % 2);
 
     // Output input by user
-    System.out.println("First Number a = " + n);
-    System.out.println("Second Number b = " + i);
+    System.out.println("First Number a = " + i);
+    System.out.println("Second Number b = " + n);
 
     // for loop  
-    for (int m = i; i <= n; ++i) {
-      System.out.println("Java is fun"+" from: "+i+ " to: "+n);
+
+    if (z <= 0);{
+        for (int m = i; i <= n; ++i) {
+    
+
+         System.out.println("Java is fun"+" from: "+i+ " to: "+n);
 
         }
     }
+    else {
+
+        System.out.println("Error");
+
+            }
+    }
 }
+
