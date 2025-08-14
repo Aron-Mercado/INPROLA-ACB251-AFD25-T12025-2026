8d39e1ecb2bbb332e52f499c52c64f2b745ca500
Aron-Mercado <agmercado@student.apc.edu.ph>
2025-08-14 02:19:02 +0000

    while loop added

--- week04/OddNumberFinder.java
+++ week04/OddNumberFinder.java
@@ -12,7 +12,7 @@ public class OddNumberFinder {
      * @param end The ending number of the range.
      */
     public void findAndDisplayOdd(int start, int end) {
-        System.out.println("\nEven numbers in the range " + start + " to " + end + " are:");
+        System.out.println("\nOdd numbers in the range " + start + " to " + end + " are:");
  
         // Use a for loop to iterate from the start point to the end point
         for (int i = start; i <= end; i++) {
@@ -23,5 +23,21 @@ public class OddNumberFinder {
                 System.out.println(i);
             }
         }
+    }
+
+        public void findAndDisplayOdd2(int start, int end) {
+        System.out.println("\nOdd numbers in the range " + start + " to " + end + " are:");
+ 
+        // Use a for loop to iterate from the start point to the end point
+        int i = start;
+        while (i <= end) {
+            // Check if the current number (i) is even.
+            // A number is even if the remainder when divided by 2 is 0.
+            if (i % 2 != 0) {
+                // If the number is even, print it to the console
+                System.out.println(i);
+        }
+        i++;
+      }
     }
 }
