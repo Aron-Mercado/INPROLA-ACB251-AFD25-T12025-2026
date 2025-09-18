c76d225dc5faac11c59d4d0b58c6581a2c52557c
Aron-Mercado <agmercado@student.apc.edu.ph>
2025-09-18 05:20:01 +0000

    added custom array size and score user input

--- week08/ArrayExample4B.java
+++ week08/ArrayExample4B.java
@@ -1,14 +1,21 @@
 
+import java.util.Scanner; 
 
 public class ArrayExample4B {
     public static void main(String[] args) {
 
+        //
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the range of scores: ");
         int size = input.nextInt();
 
         // Declare and initialize an array of integers
         int[] scores = new int[size];
+        
+        System.out.print("Enter Scores: ");
+        for (int i = 0; i < size; i++) {
+            scores[i] = input.nextInt();
+        }
 
         // Assume the first element is the maximum
         int max = scores[0];
