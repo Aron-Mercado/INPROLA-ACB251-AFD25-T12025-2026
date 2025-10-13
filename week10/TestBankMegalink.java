4503576adb431d69308a256e24eb72b85b77b04f
Aron-Mercado <agmercado@student.apc.edu.ph>
2025-10-13 02:48:23 +0000

    main file for banking exercise added

---
+++ week10/TestBankMegalink.java
@@ -0,0 +1,11 @@
+public class TestBankMegalink {
+    public static void main(String[] args) {
+        
+        PNB pnb = new PNB();
+        pnb.setBalance(1000);
+
+        pnb.balanceInquiry();
+        pnb.deposit(500);
+        pnb.withdraw(1000);
+    }
+}
